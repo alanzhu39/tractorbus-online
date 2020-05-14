9fb1345d42c4a1f60463f77a8c7049e08f6051e8
alanzhu39 <36008118+alanzhu39@users.noreply.github.com>
2020-05-14 12:03:46 -0700

    trying userID assignment

--- application.py
+++ application.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, url_for
 from flask_socketio import SocketIO, send, emit
+from single_player.round import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
-
-num_conns = 0
 
 @app.route('/')
 def index():
@@ -18,8 +17,61 @@ def message(data):
 
 @socketio.on('connected')
 def new_connection(data):
+    global num_conns
     num_conns += 1
     emit('assign_id', num_conns)
+    if num_conns == 4:
+        start_game()
+
+@socketio.on('test-event')
+def test_func(data):
+    print(data)
+
+def start_game():
+    sheng_order = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    rank_ids = [0, 0, 0, 0]
+
+    zj_id = 0
+    players = [Player("Adam", sheng_order[0]), Player("Andrew", sheng_order[0]),
+               Player("Alan", sheng_order[0]), Player("Raymond", sheng_order[0])]
+    players[zj_id].set_is_zhuang_jia(True)
+
+    while True:
+        pass
+        # r = Round(players)
+        # pts = r.play_round()
+        #
+        # if pts == 0:
+        #     # increase trump rank of players[zj_id] and players[(zj_id+2)%4] by 3
+        #     rank_ids[zj_id] += 3
+        #     rank_ids[(zj_id + 2) % 4] += 3
+        #     zj_id = (zj_id + 2) % 4
+        # elif pts < 40:
+        #     # increase trump rank of players[zj_id] and players[(zj_id+2)%4] by 2
+        #     rank_ids[zj_id] += 2
+        #     rank_ids[(zj_id + 2) % 4] += 2
+        #     zj_id = (zj_id + 2) % 4
+        # elif pts < 80:
+        #     # increase trump rank of players[zj_id] and players[(zj_id+2)%4] by 1
+        #     rank_ids[zj_id] += 1
+        #     rank_ids[(zj_id + 2) % 4] += 1
+        #     zj_id = (zj_id + 2) % 4
+        # else:
+        #     num_shengs = (pts - 80) // 40
+        #     # increase trump rank of players[(zj_id+1)%4] and players[(zj_id+3)%4] by num_shengs
+        #     rank_ids[(zj_id + 1) % 4] += num_shengs
+        #     rank_ids[(zj_id + 3) % 4] += num_shengs
+        #     zj_id = (zj_id + 1) % 4
+        #
+        # for i in range(4):
+        #     players[i].set_is_zhuang_jia(False)
+        #     players[i].set_trump_rank(sheng_order[rank_ids[i]])
+        # players[zj_id].set_is_zhuang_jia(True)
+        #
+        # if rank_ids[zj_id] >= len(sheng_order):
+        #     print(players[zj_id].get_name() + ' and ' + players[(zj_id + 2) % 4].get_name() + 'win!')
+        #     break
 
 if __name__ == "__main__":
+    num_conns = 0
     socketio.run(app, debug=True)
--- static/scripts/socketio.js
+++ static/scripts/socketio.js
@@ -1,18 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-  var socket = io();
-  let userID;
+var socket = io();
+let userID;
+console.log('test log');
 
-  socket.on('connect', function() {
-    socket.emit('connected', 'I\'m connected!');
-  });
-
-  socket.on('message', (data) => {
-    console.log(`Message received: ${data}`);
-  });
-
-  socket.on('assign_id', (num_conns) => {
-    if (!userID) {
-      userID = num_conns;
-    }
-  })
+socket.on('connect', function() {
+  console.log('im connected');
+  socket.emit('connected', 'I\'m connected!');
 });
+
+socket.on('message', (data) => {
+  console.log(`Message received: ${data}`);
+});
+
+socket.on('assign_id', (numConns) => {
+  if (typeof userID == "undefined") {
+    userID = numConns;
+  }
+  console.log(numConns);
+});
+
+function testFunc() {
+  socket.emit('test-event', 'this is a test');
+}
